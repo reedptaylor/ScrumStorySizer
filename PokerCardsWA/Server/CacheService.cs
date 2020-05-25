87d7ffd625cd75632ab85befa8b617f8302d6059
Reed Taylor <rtaylor@umail.ucsb.edu>
2020-05-24 22:43:03 -0700

    Add missing files

---
+++ PokerCardsWA/Server/CacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using PokerCardsShared.Models;
+
+namespace PokerCardsWA.Server
+{
+    public class CacheService
+    {
+        public string StoryName { get; set; }
+
+        public List<SizeVote> StorySizeVotes { get; set; } = new List<SizeVote>();
+
+        public bool ShowVotes { get; set; }
+    }
+}
