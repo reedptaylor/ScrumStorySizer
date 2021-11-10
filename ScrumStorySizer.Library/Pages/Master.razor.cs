8c72dea7d32dc493fdf825e725d51f199d04c671
Reed Taylor <reed.taylor@me.com>
2021-11-09 23:17:35 -0800

    Hook up local storage to respective pages

--- ScrumStorySizer.Library/Pages/Master.razor.cs
+++ ScrumStorySizer.Library/Pages/Master.razor.cs
@@ -1,14 +1,20 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ScrumStorySizer.Library.Models;
 using ScrumStorySizer.Library.Services;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ScrumStorySizer.Library.Pages
 {
     public partial class Master : IDisposable
     {
-
+        [Inject] protected IJSRuntime JSRuntime { get; set; }
         [Inject] IVotingService PokerVote { get; set; }
+
+        private DevOpsCredential DevOpsCredential { get; set; } = new();
 
         private bool ShowResultsDisabled
         {
@@ -38,9 +44,19 @@ namespace ScrumStorySizer.Library.Pages
             }
         }
 
-        protected override void OnInitialized()
+        protected async override Task OnInitializedAsync()
         {
             PokerVote.OnChange += OnUpdate;
+            string scrumMasterSettings = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "devops-auth");
+            if (!string.IsNullOrWhiteSpace(scrumMasterSettings))
+            {
+                try
+                {
+                    scrumMasterSettings = Encoding.UTF8.GetString(Convert.FromBase64String(scrumMasterSettings));
+                    DevOpsCredential = JsonSerializer.Deserialize<DevOpsCredential>(scrumMasterSettings) ?? new();
+                }
+                catch { }
+            }
         }
 
         public void Dispose()
--- ScrumStorySizer.Library/Pages/Team.razor.cs
+++ ScrumStorySizer.Library/Pages/Team.razor.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using ScrumStorySizer.Library.Enums;
 using ScrumStorySizer.Library.Models;
 using ScrumStorySizer.Library.Services;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ScrumStorySizer.Library.Pages
 {
     public partial class Team : IDisposable
     {
+        [Inject] protected IJSRuntime JSRuntime { get; set; }
         [Inject] IVotingService PokerVote { get; set; }
 
         private void AddVote(StorySize vote)
@@ -35,9 +39,19 @@ namespace ScrumStorySizer.Library.Pages
 
         private bool ChangeVote { get; set; } = false;
 
-        protected override void OnInitialized()
+        protected async override Task OnInitializedAsync()
         {
             PokerVote.OnChange += OnUpdate;
+            string teamMemberSettings = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "team-member-settings");
+            if (!string.IsNullOrWhiteSpace(teamMemberSettings))
+            {
+                try
+                {
+                    teamMemberSettings = Encoding.UTF8.GetString(Convert.FromBase64String(teamMemberSettings));
+                    Username = (JsonSerializer.Deserialize<TeamMemberSettings>(teamMemberSettings) ?? new()).DefaultDisplayName;
+                }
+                catch { }
+            }
         }
 
         public void Dispose()
